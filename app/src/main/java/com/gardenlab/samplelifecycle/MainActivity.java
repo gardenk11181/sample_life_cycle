f4d328f7af517bfd703953b0816429054ef32894
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-27 13:17:56 +0900

    See Life Cycle by Log

--- app/src/main/java/com/gardenlab/samplelifecycle/MainActivity.java
+++ app/src/main/java/com/gardenlab/samplelifecycle/MainActivity.java
@@ -2,7 +2,12 @@ package com.gardenlab.samplelifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Main", "onCreate called");
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Main", "onStart called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Main", "onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Main", "onDestroy called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Main", "onPause called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Main", "onResume called");
     }
 }
